4d67322f70077b8255a4d273c99cf40f5360a399
Philipp Czora <philipp.czora@cloudogu.com>
2018-05-15 11:01:32 +0200

    #11 Build inside or outside of Docker depending on environment

--- build/make/build.mk
+++ build/make/build.mk
@@ -27,7 +27,14 @@ $(HOMEDIR): $(TMPDIR)
 $(PASSWD): $(TMPDIR)
 	@echo "$(USER):x:$(UID_NR):$(GID_NR):$(USER):/home/$(USER):/bin/bash" > $(PASSWD)
 
-$(TARGET_DIR)/$(ARTIFACT_ID): dependencies $(PASSWD) $(HOMEDIR) $(TARGET_DIR)
+compile-generic:
+	go build -a -tags netgo $(LDFLAGS) -installsuffix cgo -o $(TARGET_DIR)/$(ARTIFACT_ID)
+
+compile-ci: dependencies $(PASSWD) $(HOMEDIR) $(TARGET_DIR) compile-generic
+	@echo "Built on CI server"
+
+compile-local: dependencies $(PASSWD) $(HOMEDIR) $(TARGET_DIR)
+	@echo "Building locally (in Docker)"
 	@docker run --rm \
 	 -e GOOS=linux \
 	 -e GOARCH=amd64 \
@@ -37,7 +44,15 @@ $(TARGET_DIR)/$(ARTIFACT_ID): dependencies $(PASSWD) $(HOMEDIR) $(TARGET_DIR)
 	 -v $(WORKDIR):/go/src/github.com/cloudogu/$(ARTIFACT_ID) \
 	 -w /go/src/github.com/cloudogu/$(ARTIFACT_ID) \
 	 cloudogu/golang:1.10 \
-go build -a -tags netgo $(LDFLAGS) -installsuffix cgo -o $(TARGET_DIR)/$(ARTIFACT_ID)
+  make compile-generic
+
+
+$(TARGET_DIR)/$(ARTIFACT_ID): dependencies $(PASSWD) $(HOMEDIR) $(TARGET_DIR)
+ifeq ($(ENVIRONMENT), ci)
+  $(TARGET_DIR)/$(ARTIFACT_ID): compile-ci
+else
+  $(TARGET_DIR)/$(ARTIFACT_ID): compile-local
+endif
 
 
 $(TARGET_DIR)/$(ARTIFACT_ID).sha256sum:
